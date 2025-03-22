c4227bd7ba2cfd57d8a50fbe2c3b11ba22edb2c0
Karol Janowski <karoljanowski@yahoo.com>
2025-03-22 12:08:00 +0100

    added hammerspoon config

---
+++ hammerspoon/init.lua
@@ -0,0 +1,20 @@
+-- Function to open or focus an app
+function openOrFocusApp(appName)
+    local app = hs.application.get(appName)
+    if app and app:isFrontmost() then
+        app:hide()  -- Hide app if it's already focused
+    else
+        hs.application.launchOrFocus(appName)
+    end
+end
+
+-- Bind Hyper + Key to open apps
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "T", function() openOrFocusApp("iTerm") end) -- iTerm2
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "F", function() openOrFocusApp("Firefox") end) -- Firefox
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "I", function()
+    hs.application.launchOrFocus("/Users/karoljanowski/Applications/IntelliJ IDEA Community Edition.app")
+end)
+-- Bind Hyper + C to open or focus ChatGPT Desktop Version
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "C", function()
+    openOrFocusApp("ChatGPT")  -- Replace "ChatGPT" with the exact app name as shown in Activity Monitor
+end)
