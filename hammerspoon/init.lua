f44eddb05f01f7fc4a0a4253f55b204a4753d56b
Karol Janowski <karoljanowski@yahoo.com>
2025-03-23 09:30:22 +0100

    added windows management config to hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,6 @@
+local hyper = {"cmd", "alt", "ctrl", "shift"}
+
+
 -- Function to open or focus an app
 function openOrFocusApp(appName)
     local app = hs.application.get(appName)
@@ -17,4 +20,25 @@ end)
 -- Bind Hyper + C to open or focus ChatGPT Desktop Version
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "C", function()
     openOrFocusApp("ChatGPT")  -- Replace "ChatGPT" with the exact app name as shown in Activity Monitor
+end)
+
+-- 🔲 Window Management
+hs.hotkey.bind(hyper, "Left", function()
+    local win = hs.window.focusedWindow()
+    if win then win:moveToUnit(hs.layout.left50) end
+end)
+
+hs.hotkey.bind(hyper, "Right", function()
+    local win = hs.window.focusedWindow()
+    if win then win:moveToUnit(hs.layout.right50) end
+end)
+
+hs.hotkey.bind(hyper, "Up", function()
+    local win = hs.window.focusedWindow()
+    if win then win:maximize() end
+end)
+
+hs.hotkey.bind(hyper, "Down", function()
+    local win = hs.window.focusedWindow()
+    if win then win:minimize() end
 end)
